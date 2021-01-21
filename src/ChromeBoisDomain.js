98048f6b8c56ff454f3aecf55ff1308e6588842d
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-21 18:03:37 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,7 +3,10 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+   constructor() {
+     
+      this.handleClick = this.handleClick.bind(this);
+    }
   handleMouseMove = (event) => {
      let x = event.pageX;
      let y = event.pageY;
